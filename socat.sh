e1676732588a924d54b9374a4bb217154e708fbe
Vasilis Georgitzikis <tzikis@gmail.com>
2021-07-13 00:11:07 +0300

    Update socat.sh

--- socat.sh
+++ socat.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -xv
-echo "Trying to run socat"
-socat /dev/ttyS0,raw,echo=0,crnl /dev/ttyACM0,raw,echo=0,crnl
+while true
+do
+  echo "Trying to run socat"
+  socat /dev/ttyS0,raw,echo=0,crnl /dev/ttyACM0,raw,echo=0,crnl
+	sleep 5
+done
